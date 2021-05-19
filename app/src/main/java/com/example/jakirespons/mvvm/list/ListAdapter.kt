94fc6b7882ecd0e108f0a727c26a233506f8303b
daniarfadd <daniarfadaffa@yahoo.com>
2021-05-19 12:54:30 +0700

    detail,list sudah slicing

---
+++ app/src/main/java/com/example/jakirespons/mvvm/list/ListAdapter.kt
@@ -0,0 +1,22 @@
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter : RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListAdapter.ListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListAdapter.ListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ListViewHolder() : RecyclerView.ViewHolder() {
+
+    }
+
+
+}
